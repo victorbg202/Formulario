0d6994fe933e9e669b4624274ae6730e1778a5e6
victorbg202 <72407530+victorbg202@users.noreply.github.com>
2020-10-23 20:43:00 +0200

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Formulario';
+
+  mostrarInfo: string = "";
+  mostrarInfo2: string = "";
+  erDNI = '^[0-9]{8}[A-Za-z]{1}$';
+
+  enviarDatos(nombre, dni, mensaje){
+    if(nombre.value == ""){
+      alert("El nombre esta vacio");
+    }
+
+    if(dni.match(this.erDNI)){
+      this.mostrarInfo = "DNI correcto";
+    }else{
+      this.mostrarInfo = "DNI incorrecto";
+    }
+
+    if(mensaje.length < 2){
+      this.mostrarInfo2 = "El texto es demasiado corto";
+    }
+
+  }
 }
